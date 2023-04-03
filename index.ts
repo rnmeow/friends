b408b1c15ade6e47e4a3935d96e8168fe7f4d120
郭桓桓 <hi@nekohuan.cyou>
2023-04-03 19:39:01 +0800

    chore(lint): format codebase with `deno fmt`

--- index.ts
+++ index.ts
@@ -1,14 +1,17 @@
-import { parse } from 'https://deno.land/std@0.182.0/yaml/mod.ts'
-import { join } from 'https://deno.land/std@0.182.0/path/mod.ts'
+import { parse } from "https://deno.land/std@0.182.0/yaml/mod.ts";
+import { join } from "https://deno.land/std@0.182.0/path/mod.ts";
 
-const data = parse(await Deno.readTextFile(join(Deno.cwd(), 'src/links.yml')))
+const data = parse(await Deno.readTextFile(join(Deno.cwd(), "src/links.yml")));
 
 try {
-  await Deno.mkdir(join(Deno.cwd(), 'dist'), { recursive: true })
-  await Deno.writeTextFile(join(Deno.cwd(), 'dist/links.json'), JSON.stringify(data))
+  await Deno.mkdir(join(Deno.cwd(), "dist"), { recursive: true });
+  await Deno.writeTextFile(
+    join(Deno.cwd(), "dist/links.json"),
+    JSON.stringify(data),
+  );
 } catch (err) {
-  console.error(`FATAL: ${err}`)
-  Deno.exit(1)
+  console.error(`FATAL: ${err}`);
+  Deno.exit(1);
 }
 
-console.log('YAML file successfully parsed and saved as JSON.')
+console.log("YAML file successfully parsed and saved as JSON.");
